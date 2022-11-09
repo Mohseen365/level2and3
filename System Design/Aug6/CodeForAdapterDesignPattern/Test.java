1833511ccd6a9ff671eec884348d5e8a6c9f9fa2
Mohseen365 <60885006+Mohseen365@users.noreply.github.com>
2022-11-09 13:05:14 +0530

    Update Test.java

--- System Design/Aug6/CodeForAdapterDesignPattern/Test.java
+++ System Design/Aug6/CodeForAdapterDesignPattern/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		EmployeeDetails ds = new EmployeeDetails();
-		EmployeeToPLAdapter adapter = new EmployeeToPLAdapter(ds);
+		EmployeeToPLAdapter adapter = new EmployeeToPLAdapter(ds); // EmployeeToPLAdapter apn khud bnate he baki saari classes bni hui hoti he pehle se hi EmployeeToPLAdapter ki jagah StudenToPLAdapter bhi use kr sakte he
 		Intranet intra = new Intranet(adapter);
 		
 		intra.printPhoneNumbers();
