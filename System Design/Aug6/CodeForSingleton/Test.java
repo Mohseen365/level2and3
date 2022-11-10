691d30068f95da38e0fbe3c9385aaaa81a2e1d2e
Mohseen365 <60885006+Mohseen365@users.noreply.github.com>
2022-11-10 13:38:34 +0530

    Update Test.java

--- System Design/Aug6/CodeForSingleton/Test.java
+++ System Design/Aug6/CodeForSingleton/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//Singleton s1 = new Singleton(); ye nhi kr sakte kyuki constructor private he
 		Singleton s1 = Singleton.getTheSingleInstance();
 		Singleton s2 = Singleton.getTheSingleInstance();
 		
