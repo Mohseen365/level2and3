6b68d592f1d9e9659a4edc30030aba99422d768d
Mohseen365 <60885006+Mohseen365@users.noreply.github.com>
2022-11-10 13:44:07 +0530

    Update Intranet.java

--- System Design/Aug6/CodeForAdapterDesignPattern/Intranet.java
+++ System Design/Aug6/CodeForAdapterDesignPattern/Intranet.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 // Data consumer
 public class Intranet {
-	IPhoneListSource source;
+	IPhoneListSource source; // iski jagah pr ham vo saari classes bhi le sakte he jo is interface ko implement krti he
 	
 	public Intranet(IPhoneListSource source) {
 		this.source = source;
