85d55b502b708e7c5374d22456225c584b8dc694
Mohseen365 <60885006+Mohseen365@users.noreply.github.com>
2022-11-10 13:37:43 +0530

    Update Singleton.java

--- System Design/Aug6/CodeForSingleton/Singleton.java
+++ System Design/Aug6/CodeForSingleton/Singleton.java
@@ -1,14 +1,14 @@
 package CodeForSingleton;
 
-public class Singleton {
-	private static Singleton inst;
+public class Singleton { // singleton esi class hoti he jiska sirf ek object hi hota he
+	private static Singleton inst; 
 	
-	private Singleton() {
+	private Singleton() { //isko private isliye bnaya taki iska object bhar nhi bana sake
 	}
 	
 	public static Singleton getTheSingleInstance() {
-		if(inst == null) {
-			synchronized (Singleton.class) {
+		if(inst == null) { // jab pehli baar getTheSingleInstance ko call krenge tab hi banna chahiye kyuki singleton me sirf 1 object hi hota he
+			synchronized (Singleton.class) {// jab ek thread iske ander chla gya to dusre thread ko ander aane nhi deta
 				if(inst == null) {
 					inst = new Singleton();
 				}
